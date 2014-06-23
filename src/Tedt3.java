41224eb13fec338d37095d913e618c62a5097712
chkwak@mnlsolution.com <chkwak@mnlsolution.com>
2014-06-23 18:35:16 +0900

    /...

--- src/Tedt3.java
+++ src/Tedt3.java
@@ -1,4 +1,18 @@
 
+/**<pre>
+ * 클래스 설명 (첫라인의 설명은 brief 설명)
+ * 보다 자세한 설명 (두번째 라인부터)
+ *
+ *@author : 곽채화 ( chkwak@mnlsolution.com ) 
+ *@version : 1.0
+ *@since : 2014. 6. 23.
+ *update history 
+ *-------------------------------------------------
+ *@editor : 
+ *@edit date : 
+ *@edit content :
+ *-------------------------------------------------
+</pre>*/
 public class Tedt3 {
 
 }
