124862c8143ea9305342275150db626e888011c8
chkwak@mnlsolution.com <chkwak@mnlsolution.com>
2014-06-24 13:49:25 +0900

    ,,,,,

--- src/Test1.java
+++ src/Test1.java
@@ -1,4 +1,25 @@
 
+
+/**<pre>
+ * 
+ * 
+ * 여기는 브랜치 영역입니다. 
+ * 
+ * 
+ * 
+ * 클래스 설명 (첫라인의 설명은 brief 설명)
+ * 보다 자세한 설명 (두번째 라인부터)
+ *
+ *@author : 곽채화 ( chkwak@mnlsolution.com ) 
+ *@version : 1.0
+ *@since : 2014. 6. 24.
+ *update history 
+ *-------------------------------------------------
+ *@editor : 
+ *@edit date : 
+ *@edit content :
+ *-------------------------------------------------
+</pre>*/
 public class Test1 {
 
 	public static void main(String[] args) {
