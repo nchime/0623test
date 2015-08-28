40d4eb931be819778e56ca49d1ef5dffe9180529
곽채화 <chkwak@mnlsolution.com>
2015-08-28 10:25:18 +0900

    수정 테스트

--- src/Test1.java
+++ src/Test1.java
@@ -11,6 +11,10 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		System.out.println("브랜치 테스트입니다");
+		System.out.println("브랜치 테스트입니다");
+		System.out.println("브랜치 테스트입니다");
+		System.out.println("브랜치 테스트입니다");
+		
 		
 	}
 			
